43a6dcbb51eba218f6188ed2eaf8cb933d55cf8a
Jerry Wong <jerry.wong@iproperty.com>
2018-06-22 17:29:03 +0800

    update

--- index.php
+++ index.php
@@ -69,31 +69,16 @@
         color: #64683A;
     }
 </style>
-<section id="contact-section">
-    <div class="container">
-        <div class="row">
+
+    <div class="container" style="height:90vh;">
+        <div class="row" style="top:45%; position:relative;">
             <div class="col-md-12" style="text-align:center;font-family:Century Gothic;font-size: 18em;color: #a2a3a7;text-decoration:none;">
                 <a href = '/project' >O</a> + <a href = '/vendor'>U</a>
             </div>
         </div>
     </div>
-</section>
 
-<footer id="footer">
-    <div class="container">
-        <div class="col-md-8">
 
-        </div>
-        <div class="col-md-4">
-            <!-- Social Media -->
-            <ul class="social">
-                <?php
-                  echo $socialClass->GetSocialMedia();
-                ?>
-            </ul>
-        </div>
-    </div>
-</footer> <!-- /#footer -->
 
 </body>
 </html>
